210370d180307beabbc7e6196b4f3d4cf4dc9c75
Gabriel-ISS <gabriel69iss@gmail.com>
2023-08-28 10:53:02 -0400

    First Commit

---
+++ loginTask/local_settings.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'login_task',
+        'USER': 'verwirrt',
+        'PASSWORD': 'theAnalizer2022',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
